8ee656ee22cabe2f047ff989d2aec9502d551093
Stefan Johnson <sj@scrimpycat.io>
2017-07-26 11:36:37 +1000

    Test loading ingredient data

---
+++ test/yum/data_test.exs
@@ -0,0 +1,13 @@
+defmodule Yum.DataTest do
+    use ExUnit.Case
+
+    defp tree_counter(tree, n \\ 0)
+    defp tree_counter({ :__info__, _ }, n), do: n
+    defp tree_counter({ _, tree }, n), do: tree_counter(tree, n + 1)
+    defp tree_counter(tree, n), do: Enum.reduce(tree, n, &tree_counter/2)
+
+    test "loading ingredients" do
+        ingredients = Yum.Data.ingredients()
+        assert tree_counter(ingredients) == Enum.count(Yum.Ingredient.new(ingredients))
+    end
+end
