0bbf5c02ad3b4b20888ac6bc4f6390852867b35a
Stefan Johnson <sj@scrimpycat.io>
2017-08-01 20:31:15 +1000

    Add diet struct

---
+++ lib/yum/diet.ex
@@ -0,0 +1,26 @@
+defmodule Yum.Diet do
+    @moduledoc """
+      A struct that contains all the data about a diet.
+    """
+    use Bitwise
+
+    defstruct [
+        ref: nil,
+        translation: %{},
+    ]
+
+    @type t :: %Yum.Diet{ ref: String.t, translation: Yum.Data.translation_tree }
+
+    @doc """
+      Convert to a diet struct.
+    """
+    @spec new(Yum.Data.translation_tree) :: [t]
+    def new(data) do
+        Enum.map(data, fn { key, info } ->
+            %Yum.Diet{
+                ref: "#{key}",
+                translation: info
+            }
+        end)
+    end
+end
