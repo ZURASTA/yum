8fad9c9461ca06086d5d37e97ce79bb954ab3769
Stefan Johnson <sj@scrimpycat.io>
2017-08-01 20:36:26 +1000

    Add allergen struct

---
+++ lib/yum/allergen.ex
@@ -0,0 +1,26 @@
+defmodule Yum.Allergen do
+    @moduledoc """
+      A struct that contains all the data about a allergen.
+    """
+    use Bitwise
+
+    defstruct [
+        ref: nil,
+        translation: %{},
+    ]
+
+    @type t :: %Yum.Allergen{ ref: String.t, translation: Yum.Data.translation_tree }
+
+    @doc """
+      Convert to a allergen struct.
+    """
+    @spec new(Yum.Data.allergen_tree) :: [t]
+    def new(data) do
+        Enum.map(data, fn { key, info } ->
+            %Yum.Allergen{
+                ref: "#{key}",
+                translation: info
+            }
+        end)
+    end
+end
