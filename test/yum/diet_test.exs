5aa1a1d4eee25da5c7241dd72fef09e5009ddb4f
Stefan Johnson <sj@scrimpycat.io>
2017-08-04 20:40:45 +1000

    Test diet struct

---
+++ test/yum/diet_test.exs
@@ -0,0 +1,21 @@
+defmodule Yum.DietTest do
+    use ExUnit.Case
+
+    setup do
+        %{
+            diets: %{
+                "foo" => %{ "en" => %{ "term" => "1" } },
+                "bar" => %{ "en" => %{ "term" => "2" } }
+            }
+        }
+    end
+
+    test "new/1", %{ diets: diets } do
+        assert [
+            %Yum.Diet{ ref: "bar", translation: %{ "en" => %{ "term" => "2" } } },
+            %Yum.Diet{ ref: "foo", translation: %{ "en" => %{ "term" => "1" } } }
+        ] == Enum.sort(Yum.Diet.new(diets), &(&1.ref < &2.ref))
+
+        assert [] == Yum.Diet.new(%{})
+    end
+end
